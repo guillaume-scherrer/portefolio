cadef641c61359030d57ffe1526401bb39ecf650
unknown <guillaume.scherrer@isen.fr>
2014-10-18 14:52:33 +0200

    dd# Please enter the commit message for your changes. Lines starting

--- index.php
+++ index.php
@@ -1,72 +1,37 @@
 <html>
-<head>
-   <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-	
-	<!DOCTYPE html>
-<html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
-
-    <title>Starter Template for Bootstrap</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
     <link href="starter-template.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="../../assets/js/html5shiv.js"></script>
-      <script src="../../assets/js/respond.min.js"></script>
-    <![endif]-->
+    <link rel="stylesheet"  href="bootstrap-3.2.0-dist/css/bootstrap.css" >
   </head>
 
-  <body>
 
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Project name</a>
-        </div>
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </div>
-
+    <body>
     <div class="container">
-
-      <div class="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-      </div>
-
-    </div><!-- /.container -->
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
+      <header class="row">
+        <div class="col-lg-12">
+          <p> menu </p>
+          <p> bob </p>
+        </div>
+      </header>
+      <div class="row">
+        <section class="col-lg-12">
+          Section
+          
+        </section>
+        <section class=" col-lg-12">
+          Section
+          
+        </section>
+        </div>
+      
+      <footer class="row">
+        <div class="col-lg-12">
+          Pied de page
+        </div>
+        </footer>
+        </div>
+      
+    </div>
   </body>
-</html>
-</body>
+
 </html>
